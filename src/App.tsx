1d2bc6fdd0a3e37cdaf628c80a31f055b62e7f17
burhan sozer <you@example.com>
2022-01-22 13:51:31 +0300

    4-Event props

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import PersonList from './components/PersonList';
 import Status from './components/Status';
 import Heading from './components/Heading';
 import Oscar from './components/Oscar';
+import Button from './components/Button';
+import Input from './components/Input';
 
 
 function App() {
@@ -32,13 +34,17 @@ function App() {
 
   return (
     <div className="App">
-      <Greet name="Burhan" /*messageCount={15}*/ isLoggedIn={true} />
-      {/* <Person name={personName} />
-      <PersonList names={nameList} /> */}
+      {/* <Greet name="Burhan" messageCount={15} isLoggedIn={true} />
       <Status status='loading' />
       <Oscar>
         <Heading>Placeholder Text</Heading>
-      </Oscar>
+      </Oscar> */}
+      <Button
+        handleClick={(event, id) => {
+          console.log("button clicked !", event, id);
+        }}
+      />
+      <Input value='' handleChange={(event) => console.log(event)} />
     </div>
   );
 }
