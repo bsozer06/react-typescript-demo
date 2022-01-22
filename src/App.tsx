157522be1aa512684a4d7129c1b74aaa6f6f4a48
burhan sozer <you@example.com>
2022-01-22 12:33:28 +0300

    2-basic props

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Person from './components/Person';
+import PersonList from './components/PersonList';
+
 
 function App() {
+  const personName = {
+    first: "Burhan",
+    last: "Sözer"
+  }
+
+  const nameList = [
+    {
+      first: "Ali",
+      last:"Kalpak"
+    },
+    {
+      first: "Ayşe",
+      last:"Dumrul"
+    },
+    {
+      first: "Faruk",
+      last:"Yılan"
+    }
+  ]
+
   return (
     <div className="App">
-     <Greet name="Burhan"/>
+      <Greet name="Burhan" messageCount={15} isLoggedIn={true} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 type GreetProps = {
-    name: string
+    name: string,
+    messageCount: number,
+    isLoggedIn: boolean,
 };
 
 export default function Greet(props: GreetProps) {
-    return <div>Welcome {props.name}</div>;
+    
+    if (!props.isLoggedIn) {
+        return <h1>You are not in the account now !</h1>
+    }
+    
+    return <>
+        <h2>Welcome {props.name}</h2>
+        <h4>You have {props.messageCount} unread mesagges.</h4>
+    </> 
 }
